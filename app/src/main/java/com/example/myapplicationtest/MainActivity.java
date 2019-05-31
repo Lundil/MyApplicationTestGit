9e7c828cf4cac786418896d01e285d2ec9f32b86
xlamarque <lamarque.xavier@gmail.com>
2019-05-31 11:41:31 +0200

    Comment GIT

--- app/src/main/java/com/example/myapplicationtest/MainActivity.java
+++ app/src/main/java/com/example/myapplicationtest/MainActivity.java
@@ -7,7 +7,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //GIT
+        //https://medium.com/code-yoga/how-to-link-android-studio-with-github-312037a13b99
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
